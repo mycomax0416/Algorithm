605a58081ccb98f19d4f660df73f3773cb33b4ff
mycomax0416 <mycomax0416@gmail.com>
2019-08-19 22:44:50 +0900

    2019-08-19 | Second commit

--- stack/1210_problem_ladder1.py
+++ stack/1210_problem_ladder1.py
@@ -8,30 +8,22 @@ for i in range(10):
     for j in range(100):
         array += [[0] + list(map(int, input().split())) + [0]]
 
-    y = 0
-    answer = 0
+    for test_x in range(101):
+        x = test_x
+        y = 0
 
-    for x in range(101):
         if array[y][x] == 1:
-            answer = x
-            # print(x-1)
-
             while y < 99:
+                y += 1
+
                 if array[y][x-1] == 1:
-                    while array[y][x-1] == 0:
+                    while array[y][x-1] == 1:
                         x -= 1
-                    y += 1
 
                 elif array[y][x+1] == 1:
-                    while array[y][x+1] == 0:
+                    while array[y][x+1] == 1:
                         x += 1
-                    y += 1
-
-                else:
-                    y += 1
-
-        # print(array[99][x], end=' ')
 
         if array[y][x] == 2:
-            print('#{} {}'.format(i + 1, answer-1))
+            print('#{} {}'.format(i + 1, test_x-1))
             break
