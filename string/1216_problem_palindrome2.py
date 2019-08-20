8533bc94f7674b2d68640546d3b744a1c8a95cc4
mycomax0416 <mycomax0416@gmail.com>
2019-08-20 19:57:27 +0900

    2019-08-20 | Fifth commit

--- string/1216_problem_palindrome2.py
+++ string/1216_problem_palindrome2.py
@@ -44,45 +44,43 @@ sys.stdin = open('1216_sample_input.txt', 'r')
 for i in range(10):
     test_num = int(input())
     arr = []
-    count = 0
+    answer = 0
 
     for j in range(100):
         arr += [input()]
+    # print(arr)
+    for length in range(100, 1, -1):
+        # print(length)
 
-    for i in range(100, 0, -1):
-        print(i)
-    
-    start_P = 101
-    result = 0
-    mid_process = []
-
-    while result == 0:
-        start_P -= 1
         for y in range(100):
-            for x in range(100-start_P+1):
+            for x_case in range(100 - length + 1):
+                # print(x_case)
                 word = ''
-                for p in range(start_P):
-                    word += arr[y][x+p]
+                for x in range(length):
+                    word += arr[y][x + x_case]
+                # print(word)
 
                 if word == word[::-1]:
-                    mid_process += [len(word)]
-                break
-    print(max(mid_process))
-
-
-
-
-
+                    answer += length
+                    print(answer)
+                    break
         
-#     for x in range(100):
-#         for k in range(2, P_len):
-#             for y in range(100-k+1):
-#                 word = ''
-#                 for p in range(k):
-#                     word += arr[y+p][x]
+        if answer != 0:
+                break
 
-#                 if word == word[::-1]:
-#                     if len(word) > max_word_len:
-#                         max_word_len = len(word)
-    
-#     print('#{} {}'.format(i + 1, max_word_len))
+        for x in range(100):
+            for y_case in range(100 - length + 1):
+                # print(x_case)
+                word = ''
+                for y in range(length):
+                    word += arr[y + y_case][x]
+                # print(word)
+
+                if word == word[::-1]:
+                    answer += length
+                    print(answer)
+                    break
+        
+        if answer != 0:
+                break
+
