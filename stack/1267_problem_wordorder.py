72ea4e40743935cd21e5de57cb43977bb0e55b60
mycomax0416 <mycomax0416@gmail.com>
2019-08-20 16:44:17 +0900

    2019-08-20 | Third commit

--- stack/1267_problem_wordorder.py
+++ stack/1267_problem_wordorder.py
@@ -1,2 +1,31 @@
 import sys
 sys.stdin = open('1267_sample_input.txt', 'r')
+
+for i in range(10):
+    V, E = map(int, input().split())
+    lines = list(map(int, input().split()))
+
+    path = [[] for _ in range(V+1)]
+    visit = [True]
+    visit += [False for _ in range(V)]
+
+    for j in range(E):
+        u, v = lines.pop(0), lines.pop(0)
+        path[v] += [u]
+
+    result = []
+
+    while False in visit:
+        for k in range(1, len(path)):
+            if path[k] == [] and visit[k] == False:
+                S = k
+                visit[S] = True
+                result += [S]
+                break
+
+        for n in path:
+            if S in n:
+                n.remove(S)
+    
+    final = list(map(str, result))
+    print('#{} {}'.format(i+1, ' '.join(final)))
