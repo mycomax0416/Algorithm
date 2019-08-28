d6674a8316570adca355bace6ba406b23abce409
mycomax0416 <mycomax0416@gmail.com>
2019-08-29 00:52:38 +0900

    2019-08-29 | First commit

--- stack2/4880_problem_cardgame.py
+++ stack2/4880_problem_cardgame.py
@@ -2,31 +2,45 @@ import sys
 sys.stdin = open('4880_sample_input.txt', 'r')
 
 def game(arr):
-    if len(arr) < 2:
-        return 0
+    if len(arr) == 1:
+        return arr[0]
 
+    elif len(arr) == 2:
+        if arr[0][0] == 1 and arr[1][0] == 2:
+            return arr[1]
+        elif arr[0][0] == 2 and arr[1][0] == 1:
+            return arr[0]
 
-    if len(arr) == 2:
-        if sum(arr) == 3:
-            print(2)
+        elif arr[0][0] == 1 and arr[1][0] == 3:
+            return arr[0]
+        elif arr[0][0] == 3 and arr[1][0] == 1:
+            return arr[1]
 
-        if sum(arr) == 4:
-            print(1)
+        elif arr[0][0] == 2 and arr[1][0] == 3:
+            return arr[1]
+        elif arr[0][0] == 3 and arr[1][0] == 2:
+            return arr[0]
 
-        if sum(arr) == 5:
-            print(3)
-    else:
-        return game(arr[0:len(arr)//2]) + game(arr[len(arr)//2:])
+        elif arr[0][0] == 1 and arr[1][0] == 1: # 비길때
+            return arr[0]
+        elif arr[0][0] == 2 and arr[1][0] == 2:
+            return arr[0]
+        elif arr[0][0] == 3 and arr[1][0] == 3:
+            return arr[0]
 
+    elif len(arr) == 3:
+        return game([arr[0], game([arr[1], arr[2]])])
+
+    elif len(arr) > 3:
+        return game([game(arr[0:len(arr)//2]), game(arr[len(arr)//2:])])
 
 T = int(input())
 
-for t in range(1):
+for t in range(T):
     N = int(input())
-    # print(N)
-
     arr = list(map(int, input().split()))
 
-    # print(arr[0:len(arr)//2])
-    # print(arr[len(arr)//2:])
-    game(arr)
+    for no in range(1, len(arr)+1):
+        arr += [[arr.pop(0), no]]
+
+    print('#{} {}'.format(t+1, game(arr)[1]))
