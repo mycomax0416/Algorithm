90c33f4bcc44a84b51d990eefb7e2a67bac4c7f7
mycomax0416 <mycomax0416@gmail.com>
2019-09-03 18:00:53 +0900

    2019-09-03 | Third commit

--- stack2/4881_problem_arrayminsum.py
+++ stack2/4881_problem_arrayminsum.py
@@ -1,62 +1,26 @@
 import sys
 sys.stdin = open('4881_sample_input.txt', 'r')
 
-MIN = 0
-def perm(k, n, used, cursum):
-    global MIN
-    if cursum >= MIN: return
+def backtrack(k, start):
+    global result
+    if k == len(arr):
+        print(choice)
+        result.append(choice)
 
-    if k == n:
-        MIN = min(MIN, cursum)
-        return
-
-    for i in range(n):
-        if used & (1 << i):  continue
-
-        perm(k + 1, n, used | (1 << i), cursum + arr[k][i])
-
+    else:
+        for idx in range(k, len(arr)):
+            choice[k] = arr[k][idx]
+            backtrack(k+1, idx+1)
 
 T = int(input())
-for t in range(T):
+for t in range(1):
     N = int(input())
     arr = []
     for n in range(N):
         arr.append(list(map(int, input().split())))
 
-    MIN = 0xffffff
-    perm(0, N, 0, 0)
+    choice = [''] * N
+    result = []
 
-    print("#{} {}".format(t+1, MIN))
-
-
-#-----------------------------------
-# T = int(input())
-
-# for t in range(T):
-#     N = int(input())
-#     arr = []
-
-#     for n in range(N):
-#         arr += [list(map(int, input().split()))]
-
-#     order = []
-#     cnt = 1
-    
-#     def function(k, n, used):
-#         if k == n:
-#             global cnt
-#             print(order)
-#             cnt += 1
-#             return
-
-#         for i in range(n):
-#             if used & (1<<i):
-#                 continue
-            
-#             order += arr[k-1][i-1]
-            
-#             function(k+1, n, used | (1<<i))
-
-#             order.pop()
-
-#     function(0, len(arr), 0)
+    backtrack(0, 0)
+    print(result)
