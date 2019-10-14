282d9307d5c93a08877b8bf31be42792d08a7f81
mycomax <mycomax0416@gmail.com>
2019-10-14 19:55:47 +0900

    2019-10-14 | Second commit

--- 삼성 코딩테스트 대비/16234_BOJ_인구이동.py
+++ 삼성 코딩테스트 대비/16234_BOJ_인구이동.py
@@ -1,6 +1,6 @@
-import sys
+# import sys
 from collections import deque
-sys.stdin = open('16234_BOJ_input.txt', 'r')
+# sys.stdin = open('16234_BOJ_input.txt', 'r')
 
 
 def find():
@@ -8,7 +8,6 @@ def find():
         for x in range(N):
             if visit[y][x] == False:
                 return x, y
-
 
 # 동 서 남 북
 dx = [1, -1, 0, 0]
@@ -18,7 +17,8 @@ def function(input_x, input_y):
     Queue = deque()
     Queue.append((input_x, input_y))
     visit[input_y][input_x] = True
-    group = [(input_x, input_y)]
+    group = deque()
+    group.append((input_x, input_y))
     sum_group = arr[input_y][input_x]
 
     while Queue:
@@ -34,28 +34,73 @@ def function(input_x, input_y):
                 sum_group += arr[ny][nx]
                 visit[ny][nx] = True
     
+    average = sum_group // len(group)
+    
     for x, y in group:
-        arr[y][x] = sum_group // len(group)
+        arr[y][x] = average
 
 
-T = int(input())
-for t in range(T):
-    N, L, R = map(int, input().split())
-    arr = [list(map(int, input().split())) for _ in range(N)]
+# T = int(input())
+# for t in range(T):
+N, L, R = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(N)]
 
-    move = 0
+finish = [[True]*N for _ in range(N)]
+move = -1
+count = 0
+
+while count != N*N:
     count = 0
+    visit = [[False]*N for _ in range(N)]
 
-    while count != N*N:
-        visit = [[False]*N for _ in range(N)]
-        count = 0
+    while visit != finish:
+        x, y = find()
+        count += 1
+        function(x, y)
 
-        while visit != [[True]*N for _ in range(N)]:
-            count += 1
-            x, y = find()
-            function(x, y)
+    move += 1
 
-        if count != N*N:
-            move += 1
+print(move)
 
-    print(move)
+# 정답 ---------------------------------------
+# def BFS(x, y):
+#     global N, L, R
+#     f = r = -1                          # f = front, r = rear
+#     visit[x][y] = True; S = MAP[x][y]
+#     r += 1
+#     Q[r][0], Q[r][1] = x, y
+#     while f != r:
+#         f += 1
+#         x, y = Q[f][0], Q[f][1]
+#         for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+#             tx, ty = x + dx, y + dy
+#             if 0 <= tx < N and 0 <= ty < N and not visit[tx][ty]:
+#                 if L <= abs(MAP[tx][ty] - MAP[x][y]) <= R:
+#                     visit[tx][ty] = True; S += MAP[tx][ty]
+#                     r += 1
+#                     Q[r][0], Q[r][1] = tx, ty
+#     if r > 0:
+#         S = S // (r + 1)
+#         for i in range(0, r + 1):
+#             MAP[Q[i][0]][Q[i][1]] = S
+#         return True
+#     else:
+#         return False
+# # -----------------------------------------------
+
+# N, L, R = map(int, input().split())
+# MAP = [list(map(int, input().split())) for _ in range(N)]
+# Q = [[0, 0] for _ in range(N * N)]
+
+# cnt = 0
+# while True:
+#     flag = False
+#     visit = [[False] * N for _ in range(N)]
+#     for i in range(N):
+#         for j in range(N):
+#             if not visit[i][j]:
+#                 flag |= BFS(i, j)
+
+#     if not flag: break
+#     cnt += 1
+# print(cnt)
--- 삼성 코딩테스트 대비/17143_BOJ_낚시왕.py
+++ 삼성 코딩테스트 대비/17143_BOJ_낚시왕.py
@@ -1,9 +1,37 @@
-import sys
+import sys, copy
+from pprint import pprint
 sys.stdin = open('17143_BOJ_input.txt', 'r')
 
+
+def catch(x):
+    global size_sum
+
+    for y in range(C):
+        if arr[y][x] != 0:
+            size_sum += arr[y][x][0]
+            return
+
+
 T = int(input())
-for t in range(T):
+for t in range(1):
     R, C, M = map(int, input().split())
-    arr = [list(map(int, input().split())) for _ in range(M)]
-    print(R, C, M)
-    print(arr)
+
+    arr = [[0]*(R) for _ in range(C)]
+    for _ in range(M):
+        r, c, s, d, z = map(int, input().split())
+        arr[c-1][r-1] = (s, d, z)
+
+    empty_arr = copy.deepcopy(arr)
+
+    pprint(arr)
+
+    for y in range(C):
+        for x in range(R):
+            if arr[y][x] != 0:
+                s, d, z = arr[y][x]
+                # print(s, d, z)
+
+    size_sum = 0
+
+    for idx in range(R):
+        print(idx)
