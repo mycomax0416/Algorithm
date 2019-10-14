3232a45ccde99b3fc19ee8ea9b3c991a17811c3e
mycomax0416 <mycomax0416@gmail.com>
2019-10-15 00:32:42 +0900

    2019-10-15 | First commit

--- 삼성 코딩테스트 대비/17143_BOJ_낚시왕.py
+++ 삼성 코딩테스트 대비/17143_BOJ_낚시왕.py
@@ -1,37 +1,76 @@
-import sys, copy
-from pprint import pprint
-sys.stdin = open('17143_BOJ_input.txt', 'r')
+import copy#, sys
+# sys.stdin = open('17143_BOJ_input.txt', 'r')
 
 
 def catch(x):
     global size_sum
 
-    for y in range(C):
+    for y in range(R):
         if arr[y][x] != 0:
-            size_sum += arr[y][x][0]
+            size_sum += arr[y][x][2]
+            arr[y][x] = 0
             return
 
 
-T = int(input())
-for t in range(1):
-    R, C, M = map(int, input().split())
+def move(x, y, s, d, z, k):
+    if k == 0:
+        if after_arr[y][x] != 0:
+            if after_arr[y][x][0] > s:
+                return
 
-    arr = [[0]*(R) for _ in range(C)]
-    for _ in range(M):
-        r, c, s, d, z = map(int, input().split())
-        arr[c-1][r-1] = (s, d, z)
+        else:
+            after_arr[y][x] = (s, d, z)
 
-    empty_arr = copy.deepcopy(arr)
 
-    pprint(arr)
+    else:
+        if d == 1 and 0 <= y < R:   # 위
+            if y == 0:
+                move(x, y+1, s, 2, z, k-1)
+            else:
+                move(x, y-1, s, 1, z, k-1)
 
-    for y in range(C):
-        for x in range(R):
+        elif d == 2 and 0 <= y < R: # 아래
+            if y == R-1:
+                move(x, y-1, s, 1, z, k-1)
+            else:
+                move(x, y+1, s, 2, z, k-1)
+
+        elif d == 3 and 0 <= x < C: # 오른쪽
+            if x == C-1:
+                move(x-1, y, s, 4, z, k-1)
+            else:
+                move(x+1, y, s, 3, z, k-1)
+
+        elif d == 4 and 0 <= x < C:
+            if x == 0:
+                move(x+1, y, s, 3, z, k-1)
+            else:
+                move(x-1, y, s, 4, z, k-1)
+
+
+# T = int(input())
+# for t in range(T):
+R, C, M = map(int, input().split())
+
+arr = [[0]*(C) for _ in range(R)]
+empty_arr = copy.deepcopy(arr)
+
+for _ in range(M):
+    r, c, s, d, z = map(int, input().split())
+    arr[r-1][c-1] = (s, d, z)
+
+size_sum = 0
+
+for idx in range(C):
+    catch(idx)
+    after_arr = copy.deepcopy(empty_arr)
+
+    for y in range(R):
+        for x in range(C):
             if arr[y][x] != 0:
                 s, d, z = arr[y][x]
-                # print(s, d, z)
+                move(x, y, s, d, z, s)
+    
+    arr = after_arr[:]
 
-    size_sum = 0
-
-    for idx in range(R):
-        print(idx)
+print(size_sum)
